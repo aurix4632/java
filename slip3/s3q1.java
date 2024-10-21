ab43f03395c9495a4e7bd58304d2305efe04c8b6
aurix4632 <aurix4632@gmail.com>
2024-10-21 19:38:04 +0530

    Update s3q1.java

--- slip3/s3q1.java
+++ slip3/s3q1.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-public class s3q1
-{
-int sum=0;
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the number of array elements : ");
-int n=sc.nextInt();
-int a[]=new int[n];
-for(int i=0;i<a.length;i++)
-{
-sum=sum+a[i];
-}
-System.out.println("Sum of array elements :"+sum);
+public class que3{
+    public static void main(String[] args){
+    int sum=0;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number of array element");
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    System.out.println("Enter the elements");
+    for(int i=0;i<a.length;i++){
+        a[i]=sc.nextInt();
+    }
+    for(int i=0;i<a.length;i++){
+        sum+=+a[i];
+    }
+    System.out.println("sum of array elements"+sum);
 }
 }
