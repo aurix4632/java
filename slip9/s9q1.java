426dba2ede6819c21dcbe534ec7448f9c23a2b57
aurix4632 <aurix4632@gmail.com>
2024-10-22 19:46:29 +0530

    Update s9q1.java

--- slip9/s9q1.java
+++ slip9/s9q1.java
@@ -1,20 +1,25 @@
-import java.io.*;
-class s9q1
-{
-public static void main(String[] args)
-{
-int sum=0;
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-System.out.println("Enter a number : ");
-int n=Integer.parseInt(br.readLine());
-for(int i=0;i<n;i++)
-{
-if(n%i==0)
-sum=sum+i;
-}
-if(sum==n)
-System.out.println("Number is perfect");
-else
-System.out.println("Number is not perfect");
-}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class que9 {
+    public static void main(String[] args) throws IOException {
+        int sum = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Enter a number:");
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println(n + " is a perfect number.");
+        } else {
+            System.out.println(n + " is not a perfect number.");
+        }
+    }
 }
